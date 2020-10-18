538ec6a4ebc45cbc172a668430a13556cb3569ba
Rainer <rainerresende@gmail.com>
2020-10-18 16:41:09 -0300

    Primeiro commit

---
+++ TakeBot/Controllers/RepositoriosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TakeAPI.Models;
+
+namespace TakeBot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RepositoriosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+             return Ok(IntegrarAPIGitHub());
+
+        }
+
+        public List<RepositorioModel> IntegrarAPIGitHub()
+        {
+            List<RepositorioModel> repositorio = new List<RepositorioModel>();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+                HttpResponseMessage response = client.GetAsync("https://api.github.com/orgs/takenet/repos").Result;
+
+                response.EnsureSuccessStatusCode();
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+                
+                foreach (var item in resultado)
+                {
+                    RepositorioModel repos = new RepositorioModel();
+
+                    repos.NomeCompleto = item.full_name;
+                    repos.DescricaoRepositorio = item.description;
+                    repos.Imagem = item.owner.avatar_url;
+                    repos.DataCriacao = item.created_at;
+                    repos.linguagemRepositorio = item.language.ToString();
+
+                    repositorio.Add(repos);
+                }
+
+                return repositorio.Where(b => b.linguagemRepositorio.Equals("C#")).OrderBy(a => a.DataCriacao).Take(5).ToList();
+            }
+        }
+    }
+}
---
+++ TakeBot/Models/RepositorioModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TakeAPI.Models
+{
+    public class RepositorioModel
+    {
+        public string Imagem { get; set; }
+        public string NomeCompleto { get; set; }
+        public string DescricaoRepositorio { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public string linguagemRepositorio { get; set; }
+    }
+}
